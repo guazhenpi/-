d6c700a4f9adb0b09b8f63d494b8b1b355b0d50e
张宇 <651359790@qq.com>
2020-07-16 15:08:18 +0800

    三角波刷新

--- main.cpp
+++ main.cpp
@@ -1,14 +1,18 @@
 #include <QApplication>
 #include <QWidget>
 
-#include "layoutDemo.h"
+//#include "layoutDemo.h"
+#include "qwidgetdraw.h"
 
 int main(int argc, char * argv[])
 {
     QApplication a(argc,argv);
 
+    QWidgetDraw *drawWidget = new QWidgetDraw();
+    drawWidget->setWindowTitle("暑假实习起飞组");
+    drawWidget->resize(600,150);
 
-    layoutDemo2();
+    drawWidget->show();
 
     return a.exec();
 }
